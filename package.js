17e68750aaf0fca78695f311762592e0daff5250
Pete C <cretep@users.noreply.github.com>
2014-09-01 11:05:11 +0100

    git info

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Provides jquery.floatThead",
   version: "1.0.0",
-  git: " \* Fill me in! *\ "
+  git: "https://github.com/cretep/meteor-jquery-floatthead/tree/v1.2.8"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
   api.use('jquery', 'client');
-  api.addFiles('floatThead/dist/jquery.floatThead.js');
+  api.addFiles('floatThead/dist/jquery.floatThead.js', 'client');
 });
